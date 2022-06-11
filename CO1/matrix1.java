649e3eeffa792877c1dc3fd480ed20dcf2356dcb
Gopika-S-Raj <94900963+Gopika-S-Raj@users.noreply.github.com>
2022-06-12 00:06:43 +0530

    Update matrix1.java

--- CO1/matrix1.java
+++ CO1/matrix1.java
@@ -1,10 +1,20 @@
 package co;
 
-public class matrix1 {
 
-	public matrix1() {
-		int a[][]= {{1,2,3},{5,6,5},{3,20,2}};
-		int b[][]= {{7,8,6},{3,4,5},{32,3,5}};
+public class matrix {
+
+	public static void main(String[] args) {
+		int a[][]= {{1,2,3},{4,5,6},{7,8,9}};
+		int b[][]= {{0,1,2},{3,4,5},{6,7,8}};
+		int c[][]=new int[3][3];
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				c[i][j]=a[i][j]+b[i][j];
+				System.out.print(c[i][j] +" ");
+			}
+			System.out.println();
+		}
+
 	}
 
 }
